b873cb51de7bab75d4cde251f900ef59d126b4c8
Matthew Davis <matthew@matthewdavis.io>
2019-03-16 02:01:00 -0500

    docs

---
+++ src/Commands/PingCommand.ts
@@ -0,0 +1,60 @@
+import { CommandBase }   from '../Common/CommandBase';
+import { Command }       from '../Common/CommandDecorator';
+import { CommandParser } from '../Common/CommandParser';
+
+/**
+ * Replies back to the user with "pong!"
+ */
+@Command
+export class PingCommand extends CommandBase {
+
+    public constructor() {
+
+        //
+        // Set this commands configuration.
+        //
+        super({
+
+            name: '>test',
+            description: 'Simple test command that sends a reply if validation succeeds.',
+            params: [ {
+
+                name: 'chars',
+                description: 'An argument containing a word.',
+                required: true,
+                pattern: '\\w+'
+
+            }, {
+
+                name: 'num',
+                description: 'An argument containing numbers.',
+                required: true,
+                pattern: '\\d+'
+
+            }, {
+
+                name: 'opt',
+                description: 'An optional argument.',
+                required: false,
+
+            } ]
+
+        });
+
+    }
+
+    /**
+     * Called when a command matches config.name.
+     *
+     * @param command Parsed out commamd
+     *
+     */
+    public run(command: CommandParser): void {
+
+        // console.log(command);
+
+        command.message.reply('Test received!');
+
+    }
+
+}
